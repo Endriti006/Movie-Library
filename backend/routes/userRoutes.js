1fae70baec2873974dd8a04503f555c40a3d9323
Endriti Kryeziu <endriti006@gmail.com>
2024-12-17 19:27:08 +0100

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,22 +11,22 @@ const {
 const { loginUser } = require("../controllers/authController");
 const passport = require("passport");
 
-// Login route (public)
+
 router.post("/login", loginUser);
 
-// Protected route for getting all users (requires JWT authentication)
+
 router.get("/", passport.authenticate("jwt", { session: false }), getAllUsers);
 
-// Route to create a new user
+
 router.post("/", createUser);
 
-// Route to get user by username
+
 router.get("/:username", getUserByUsername);
 
-// Route to update user by username
+
 router.put("/:username", updateUserByUsername);
 
-// Route to delete user by username
+
 router.delete("/:username", deleteUserByUsername);
 
 module.exports = router;
