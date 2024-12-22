a953ce85ff1b87b9d0930250095d1e4a90183b2f
Endriti Kryeziu <93491718+Endriti006@users.noreply.github.com>
2024-12-22 23:35:40 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../database/db");
 
+
 const User = sequelize.define(
   "users",
   {
@@ -46,4 +47,114 @@ const User = sequelize.define(
   }
 );
 
-module.exports = User;
+// Genre Model
+const Genre = sequelize.define(
+  "genres",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: "Genre name must be unique",
+      },
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+
+const Movie = sequelize.define(
+  "movies",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    releaseDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    genreId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Genre,
+        key: "id",
+      },
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+
+const Rating = sequelize.define(
+  "ratings",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+    movieId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Movie,
+        key: "id",
+      },
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+
+Genre.hasMany(Movie, { foreignKey: "genreId" });
+Movie.belongsTo(Genre, { foreignKey: "genreId" });
+
+Movie.hasMany(Rating, { foreignKey: "movieId" });
+Rating.belongsTo(Movie, { foreignKey: "movieId" });
+
+User.hasMany(Rating, { foreignKey: "userId" });
+Rating.belongsTo(User, { foreignKey: "userId" });
+
+module.exports = { User, Genre, Movie, Rating };
